3a6749e3487ebe92dce156024dd261142b1a6d7b
Masha <marusea_rod@list.ru>
2020-03-12 17:15:28 +0300

    add modal helper

--- src/modules/modal/modal-helper.js
+++ src/modules/modal/modal-helper.js
@@ -3,7 +3,7 @@ class modalHelper {
         document.addEventListener('click', (event) => {
             event.preventDefault();
             const target = event.target;
-           // console.log(target);
+            //console.log(target);
             if(target.dataset.popup) {
                 const id = target.dataset.popup;
                 document.querySelector(id).style.cssText = 'display: flex';
@@ -12,7 +12,7 @@ class modalHelper {
                 document.querySelector(id).style.cssText = 'display: flex';
             }
 
-            if(target.closest('.close-form') || !target.closest('.form-wrapper')) {
+            if(target.closest('.close-form') || target.matches('.overlay')) {
                 target.closest('.popup').style.cssText = '';
             }
 
